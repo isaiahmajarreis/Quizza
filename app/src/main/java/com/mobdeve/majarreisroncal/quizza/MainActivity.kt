ac33ab4376f44a0f567b8a6bed1d098e92542a37
isaiah majarreis <80748304+isaiahmajarreis@users.noreply.github.com>
2022-05-23 20:43:52 +0800

    Added a splash screen upon app startup

--- app/src/main/java/com/mobdeve/majarreisroncal/quizza/MainActivity.kt
+++ app/src/main/java/com/mobdeve/majarreisroncal/quizza/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.mobdeve.majarreisroncal.quizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val i = Intent(this@MainActivity, MenuActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 2000)
     }
 }
