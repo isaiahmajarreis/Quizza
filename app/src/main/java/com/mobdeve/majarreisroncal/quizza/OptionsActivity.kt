bf901bc605dba9143c113e845282d05f34430264
raphroncal <raph.roncal01@gmail.com>
2022-07-04 20:18:08 +0800

    logout

--- app/src/main/java/com/mobdeve/majarreisroncal/quizza/OptionsActivity.kt
+++ app/src/main/java/com/mobdeve/majarreisroncal/quizza/OptionsActivity.kt
@@ -1,21 +1,32 @@
 package com.mobdeve.majarreisroncal.quizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.mobdeve.majarreisroncal.quizza.databinding.ActivityOptionsBinding
 
 class OptionsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOptionsBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOptionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        binding.tvEmail.text = user?.email.toString()
+
         // TODO: back button for the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Options"
 
-        // TODO: logout dialog alert ("Are you sure you want to log out?")
+        binding.tvLogout.setOnClickListener {
+            auth.signOut()
+            val goToLogin = Intent(this, LoginActivity::class.java)
+            startActivity(goToLogin)
+        }
     }
 }
